ad56b13140f6a322e977d6feaef651cf29f5f712
HonbraOfficial <54026315+HonbraOfficial@users.noreply.github.com>
2020-01-28 17:12:28 +0100

    Added material ripples

--- index.js
+++ index.js
@@ -410,3 +410,10 @@
     }
 
 })();
+
+var mdc = mdc;
+
+const buttons = document.querySelectorAll(".mdc-ripple-surface");
+for (const button of buttons) {
+  mdc.ripple.MDCRipple.attachTo(button);
+}
