ee60940c9e1b174a3f0b8a92fb27dc387313e941
Maty1000 <55660550+Maty1000@users.noreply.github.com>
2020-04-01 13:47:15 +0200

    Experiment with editing description.
    Tested editing descriptions on IVT.

--- data.js
+++ data.js
@@ -97,7 +97,7 @@ var APP_DATA = {
           "yaw": -0.16753181259830363,
           "pitch": 0.024986385922334264,
           "title": "Počítačová laboratoř",
-          "text": "V počítačové laboratoří děláme různé věci na počítačích."
+          "text": "V počítačové laboratoři se učí IVT (informační a výpočetní technologe)."
         }
       ]
     },
